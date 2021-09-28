85b01c923339897094d6c9d4822f2f062fb84489
hitman <mrezaee79@gmail.com>
2021-09-28 15:22:02 -0700

    update config 1.1

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-   target: 'static',
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   router: {
     base: '/sofa/',
