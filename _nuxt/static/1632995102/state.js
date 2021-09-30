a4d96f6c943a7e92613e8d0ed39cc47ee82fd3d0
hitman00 <hitman00@users.noreply.github.com>
2021-09-30 09:45:05 +0000

    deploy: 221165a9a213a4c71b9cbd3cb26642c45dc77427

---
+++ _nuxt/static/1632995102/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsofa\u002F_nuxt\u002Fstatic\u002F1632995102",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsofa\u002F",assetsPath:"\u002Fsofa\u002F_nuxt\u002F",cdnURL:a}}}}(null));
